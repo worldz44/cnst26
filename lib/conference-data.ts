a83a39f9df7f630b4647ba31276e68527e0cdd8c
worldz44 <139063266+worldz44@users.noreply.github.com>
2026-07-12 16:56:40 +0100

    Fix syntax error in conferenceChairman object

--- lib/conference-data.ts
+++ lib/conference-data.ts
@@ -96,7 +96,7 @@ export const honoraryPresidents = [
 
 export const conferenceChairman = {
   {name: "Dr. KACI Meziane", role: "University of Khemis Miliana"},
-  {name: "Dr. ROUABAH Slim", role: "University of Khemis Miliana"}
+  {name: "Dr. ROUABAH Slim", role: "University of Khemis Miliana"},
 };
 
 export const scientificCommittee = {
